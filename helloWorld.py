a3aa78e51bb9f19d7436dfaf6a153737a2be2380
Fantasy <805841483@qq.com>
2018-03-11 15:59:17 +0800

    python基础

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,62 @@
-name = input()
+# 整型
+print(100)
+# 整型16进制
+print(0xff00)
+# 浮点型
+print(1.3)
+# 浮点型表示科学计数法
+print(1.23e9 == 12.3e8)
 
-print(name)
+# 表示字符串
+print("hello this is my first world")
+
+print('that\'s my world')
+
+print('this is \t my world')
+
+print('this is \n my world')
+
+print(r'this is \n my world')
+
+print('''this is 
+ my 
+ world,
+ hello 
+ world
+''')
+
+print("""this is 
+ my 
+ world,
+ hello 
+ world
+""")
+
+print(r'''hello,\n
+world''')
+
+# 布尔值运算
+print(not True)
+
+# 变量
+_de = 2
+de = 3
+print(_de)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+print(9/3)
+
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+print(s4)
